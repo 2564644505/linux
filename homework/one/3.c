ce4028051baac5c8dfbbabfe51acabe8b1dc4365
hxl <2564644505@qq.com>
2020-11-02 14:38:28 +0800

    thrid

--- homework/one/3.c
+++ homework/one/3.c
@@ -8,13 +8,18 @@
 #include <stdio.h>
 #include <float.h>
 int main(void){
-  int a = 1;
+  unsigned int a = 1;
+  int *p = NULL;
   a = a << sizeof(int)*8 - 1;
-  printf("int类型的最大值=%d\n",-a-1);
-  printf("int类型的最小值=%d\n",a);
-  printf("float类型的最大值=%e\n",FLT_MAX);
-  printf("float类型的最小值=%e\n",FLT_MIN);
+  float f = 111.3456f;
+  p = (int *)&f;
+  double d = 111.34567d;
+  printf("%u =  %XH\n",a,a);
+  printf("%G = %x\n",f,*p);
+  p = (int *)&d;
+  printf("%G = %x\n",d,*p);
+  /*printf("float类型的最小值=%e\n",FLT_MIN);
   printf("double类型的最大值=%e\n",DBL_MAX);
-  printf("double类型的最小值=%e\n",DBL_MIN);   
+  printf("double类型的最小值=%e\n",DBL_MIN);*/   
   return 0;
 }
